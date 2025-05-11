7bef19a9d23a8791db66d3e88533c1e5de214a07
Nikoloz-asanidze <asanidzenikoloz1010@gmail.com>
2025-05-11 23:30:03 +0400

    nav update and modern styles

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,51 +1,45 @@
 "use client";
 import Link from "next/link";
-import styles from "./page.module.css";
 import { usePathname } from "next/navigation";
+import styles from "./page.module.css";
 
-function layout({ children }) {
+function Layout({ children }) {
   const pathname = usePathname();
 
-    return (
-      <div className={styles.container}>
-        <nav className={styles.nav}>
+  return (
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>Asana's Market</div>
+        <div className={styles.navLinks}>
           <Link
-            className={`
-              ${styles.link}
-              ${
-                pathname.includes ("/products") ? styles.activeLink : styles.inActiveLink
-              }`
-            }
+            className={`${styles.link} ${
+              pathname.includes("/products") ? styles.activeLink : ""
+            }`}
             href="/products"
           >
             Products
           </Link>
-          <Link 
-            className={`
-              ${styles.link}
-              ${
-                pathname.includes("/profile") ? styles.activeLink : styles.inActiveLink
-              }`
-            }
+          <Link
+            className={`${styles.link} ${
+              pathname.includes("/profile") ? styles.activeLink : ""
+            }`}
             href="/profile"
           >
             Profile
           </Link>
-          <Link 
-            className={`
-              ${styles.link}
-              ${
-                pathname.includes("/cart") ? styles.activeLink : styles.inActiveLink
-              }`
-            }
+          <Link
+            className={`${styles.link} ${
+              pathname.includes("/cart") ? styles.activeLink : ""
+            }`}
             href="/cart"
           >
             Cart
           </Link>
-        </nav>
-        {children} 
-      </div>
-    );
+        </div>
+      </nav>
+      {children}
+    </div>
+  );
 }
 
-export default layout;
+export default Layout;
